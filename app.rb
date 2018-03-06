00da67efbf735385ccc630cb135080949301f941
Somayeh <mail.somayeh@gmail.com>
2018-03-06 14:52:25 +0000

    sinatra adding HTMLexit

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   'hello!'
+end
+
+get '/secret' do
+  'This is a secret page.'
+end
+
+get '/secret/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
